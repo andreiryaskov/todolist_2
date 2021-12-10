5cee15fa9cd5303d02b7564239fe888c0f09dfde
andreiryaskov <andreiryaskov@gmail.com>
2021-12-10 17:11:44 +0900

    todo_2_addTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import Todolist from "./Component/Todolist";
 import {v1} from "uuid";
 
-
-
-let tasks = [
-    {id: v1(), title: 'HTML5', isDone: true},
-    {id: v1(), title: 'CSS3', isDone: true},
-    {id: v1(), title: 'JavaScript', isDone: true},
-    {id: v1(), title: 'React', isDone: false},
-    {id: v1(), title: 'Redux', isDone: false}
-]
-
 function App() {
+
+    let [tasks, setTasks] = useState([
+        {id: v1(), title: 'HTML5', isDone: true},
+        {id: v1(), title: 'CSS3', isDone: true},
+        {id: v1(), title: 'JavaScript', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+        {id: v1(), title: 'Redux', isDone: false}
+    ])
+
+    function addTask(inputValue: string) {
+        let task = {id: v1(), title: inputValue, isDone: false}
+        let newTask = [task, ...tasks]
+        setTasks(newTask)
+    }
+
     return(
         <div className={'app_wrapper'}>
-          <Todolist tasks={tasks} title={'What to learn'}/>
+          <Todolist tasks={tasks} title={'What to learn'} addTask={addTask}/>
         </div>
     )
 }
--- src/Component/Todolist.tsx
+++ src/Component/Todolist.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './Todolost.module.css';
 type TaskType = {
     id: string
@@ -9,6 +9,7 @@ type TaskType = {
 type PropsType = {
     tasks: TaskType[]
     title: string
+    addTask: (inputValue: string) => void
 }
 
 function Todolist(props: PropsType) {
@@ -17,11 +18,13 @@ function Todolist(props: PropsType) {
         return <li><input type="checkbox"/>{t.title} <button>x</button></li>
     })
 
+    let [inputValue, setInputValue] = useState('')
+
     return(
         <div>
             <h3>{props.title}</h3>
-            <input type="text"/>
-            <button>+</button>
+            <input type="text" value={inputValue} onChange={ (e) => { setInputValue(e.currentTarget.value) } }/>
+            <button onClick={ () => {props.addTask(inputValue)} }>+</button>
             <ul className={style.listItem}>
                 {taskItem}
             </ul>
