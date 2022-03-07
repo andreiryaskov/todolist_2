be61fcc3666175ed7689a886aa320f5991f239de
andreiryaskov <andreiryaskov@gmail.com>
2022-03-07 21:11:46 +1000

    axios

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,19 +1,27 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios';
 
 export default {
     title: 'API'
 }
 
-export const GetTodolists = () => {
+const settings = {
+    withCredentials: true
+}
+export const GetTodoLists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        // здесь мы будем делать запрос и ответ закидывать в стейт.
-        // который в виде строки будем отображать в div-ке
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                setState(res.data);
+            })
 
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
 }
+
+
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
