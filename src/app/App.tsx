e527b429e8e953b821f48aa1880ccc3a63308e87
andreiryaskov <andreiryaskov@gmail.com>
2022-04-21 12:52:47 +0300

    depl3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -64,9 +64,9 @@ function App({demo = false}: PropsType) {
                 <Container fixed>
                     <Routes>
                         <Route path='/' element={<TodolistsList demo={demo}/>}/>
-                        <Route path='login' element={<Login/>}/>
+                        <Route path='/login' element={<Login/>}/>
                         <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
-                        <Route path='*' element={<Navigate to='/404'/>}/>
+                        <Route path='/*' element={<Navigate to='/404'/>}/>
                     </Routes>
                 </Container>
             </div>
